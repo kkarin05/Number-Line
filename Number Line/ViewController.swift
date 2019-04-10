2b3277f3a703881b92acd4b0dedbcb59e49a44de
Tian Liu <8xiaolong8@gmail.com>
2019-04-10 09:47:32 -0400

    4/10 updates

--- Number Line/ViewController.swift
+++ Number Line/ViewController.swift
@@ -58,6 +58,8 @@ class ViewController: UIViewController {
     @IBAction func handlepan(recognizer:UIPanGestureRecognizer) {
         let translation = recognizer.translation(in:self.view)
         if let view = recognizer.view {
+            let astronaut_position = astronaut.center.x
+            print("astronaut is at:" + astronaut_position.description)
             view.center = CGPoint(x:view.center.x + translation.x, y:view.center.y + translation.y)
             
         }
